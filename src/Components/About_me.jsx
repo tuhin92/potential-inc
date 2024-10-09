6d775425f5e467f6d07be0ced6aeaf72d4276a61
tuhin92 <tuhinalmamun71m@gmail.com>
2024-10-09 19:14:31 +0600

    add about me section

---
+++ src/Components/About_me.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const ManualProgressBar = ({ value, label }) => {
+    return (
+        <div className="my-4">
+            <div className="flex justify-between mb-1">
+                <span>{label}</span>
+            </div>
+            <div className="w-full bg-gray-200 h-2 rounded-full">
+                <div
+                    className="bg-[#FD6F00] h-2 rounded-full"
+                    style={{ width: `${value}%` }}
+                ></div>
+            </div>
+        </div>
+    );
+};
+
+const About_me = () => {
+    const [progressValues] = useState([
+        { label: 'UX', value: 90 },
+        { label: 'Website Design', value: 80 },
+        { label: 'App Design', value: 95 },
+        { label: 'Graphics Design', value: 75 },
+    ]);
+  return (
+    <div>
+      <div className="font-poppins py-6 md:py-20 px-6 md:px-0 text-[#000000]">
+        <div className="flex flex-col lg:flex-row justify-between items-center">
+
+        <div className="border border-red-200 h-[300px] w-[300px] md:h-[450px] md:w-[450px] rounded-full mt-8 lg:mt-0 relative">
+              <img
+                src="./second.png"
+                className="absolute inset-0 h-full w-full rounded-full"
+                alt="Profile"
+              />
+            </div>
+
+          <div className="text-center lg:text-left">
+            <h4 className="text-[#000000] font-semibold text-[20px] md:text-[52px]">
+              About Me
+            </h4>
+            <p className="py-6 text-base md:text-xl text-[#000000]">
+            Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa <br /> nibh lectus netus in. Aliquet donec morbi convallis pretium. Turpis <br /> tempus pharetra
+            </p>
+            <div className='w-full md:w-4/5 '>
+                    {progressValues.map((progress, index) => (
+                        <ManualProgressBar key={index} value={progress.value} label={progress.label} />
+                    ))}
+                </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About_me;
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../Components/Banner';
+import About_me from '../../Components/About_me';
 
 const Home = () => {
     return (
         <div className='max-w-7xl mx-auto'>
             <Banner></Banner>
+            <About_me></About_me>
         </div>
     );
 };
