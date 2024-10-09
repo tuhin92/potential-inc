b173d70de37440a3d1f3a7a75d5f6a14d746898b
tuhin92 <tuhinalmamun71m@gmail.com>
2024-10-09 14:54:59 +0600

    initial setup

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/main";
+import Home from "./Home/Home";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/foods',
+            }
+        ]
+    },
+]);
+
+export default routes;
