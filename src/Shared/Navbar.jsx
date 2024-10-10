6940cc6864ac7c9115bc7c597d84d91db4490978
tuhin92 <tuhinalmamun71m@gmail.com>
2024-10-10 20:26:21 +0600

    make the navbar sticky and smooth navigation

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-white max-w-7xl mx-auto py-8 font-poppins">
+    <div className="navbar bg-white max-w-7xl mx-auto py-8 font-poppins sticky top-0 z-50">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
